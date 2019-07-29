111d44c2194ecc408efc812e47f752ed85b9ffb4
Tezwinder Singh Tezy <52675356+TezwinderTezy31@users.noreply.github.com>
2019-07-29 01:54:33 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,49 @@
+import { songsInfo } from '../../app/songsInfo';
+
+
+export const SONG:songsInfo[] =
+  [{
+    songname: 'EAST SIDE FLOW',
+    artist: 'Sidhu Moosewala',
+    genre: 'Rock',
+    release: '2019',
+    image: '../../assets/images/Sidhu.jpg'
+  },
+
+  {
+    songname: '315',
+    artist: 'Sucha Yaar',
+    genre: 'Rock',
+    release: '2019',
+    image: '../../assets/images/suchayaar.jpg'
+  },
+
+  {
+    songname: 'EAST SIDE FLOW',
+    artist: 'Sultaan',
+    genre: 'Rock',
+    release: '2019',
+    image: '../../assets/images/sultan.jpg'
+  },
+
+  {
+    songname: 'Sachi Taksal',
+    artist: 'Jagowal Jatha',
+    genre: 'Rock',
+    release: '2017',
+    image: '../../assets/images/taksal.jpg'
+  }];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
